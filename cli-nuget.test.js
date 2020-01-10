94a758d1b18e8eda565988dc0f62dc1e42508bd5
Brandon Scott <bscott@andculture.com>
2020-01-10 15:43:32 -0500

    cli-24 Added first set of tests for nugetUpgrade command, validatePackageName()

--- cli-nuget.test.js
+++ cli-nuget.test.js
@@ -1,6 +1,48 @@
+/**************************************************************************************************
+ * Imports
+ **************************************************************************************************/
+const cli_nuget = require("./cli-nuget");
+const shell     = require("shelljs");
 
-describe("nuget", () => {
-    test("when x, it returns y", () => {
-        expect(true).toBe(true);
-    })
-})
+
+/**************************************************************************************************
+ * Variables
+ **************************************************************************************************/
+const sut = cli_nuget.nugetUpgrade;
+
+describe("nugetUpgrade", () => {
+
+    let shellExitSpy;
+
+    beforeEach(() => {
+        shellExitSpy = jest.spyOn(shell, "exit").mockImplementation(() => {});
+    });
+
+    /**************************************************************************************************
+     * validatePackageName()
+     **************************************************************************************************/
+
+    describe("validatePackageName()", () => {
+        test.each`
+            input
+            ${undefined}
+            ${null}
+            ${""}
+            ${" "}
+        `("when given '$input' as a package name, it calls shell.exit", ({ packageName }) => {
+            // Arrange & Act
+            sut.validatePackageName(packageName);
+
+            // Assert
+            expect(shellExitSpy).toHaveBeenCalledWith(-1);
+        });
+
+        test("when given a string with >= 1 characters, it does not call shell.exit", () => {
+            // Arrange & Act
+            sut.validatePackageName("AutoMapper");
+
+            // Assert
+            expect(shellExitSpy).not.toHaveBeenCalled();
+        });
+    });
+});
