e1af885e76fe4f2cf7ca0cde20d6781b0cff365b
Brandon Scott <bscott@andculture.com>
2020-03-11 15:31:42 -0400

    extract-tested Wrapped cli-webpack in run()

--- cli-webpack.js
+++ cli-webpack.js
@@ -1,84 +1,79 @@
 #!/usr/bin/env node
+require("./command-runner").run(async () => {
+    /**************************************************************************************************
+     * Imports
+     **************************************************************************************************/
 
-/**************************************************************************************************
- * Imports
- **************************************************************************************************/
+    const commands       = require("./_modules/commands");
+    const dir            = require("./_modules/dir");
+    const echo           = require("./_modules/echo");
+    const frontendPath   = require("./_modules/frontend-path");
+    const nodeClean      = require("./_modules/node-clean");
+    const nodeRestore    = require("./_modules/node-restore");
+    const program        = require("commander");
+    const shell          = require("shelljs");
+    const webpackPublish = require("./_modules/webpack-publish");
 
-const commands       = require("./_modules/commands");
-const dir            = require("./_modules/dir");
-const echo           = require("./_modules/echo");
-const frontendPath   = require("./_modules/frontend-path");
-const nodeClean      = require("./_modules/node-clean");
-const nodeRestore    = require("./_modules/node-restore");
-const program        = require("commander");
-const shell          = require("shelljs");
-const webpackPublish = require("./_modules/webpack-publish");
+    /**************************************************************************************************
+     * Commands
+     **************************************************************************************************/
 
+    // #region Webpack commands
 
-/**************************************************************************************************
- * Variables
- **************************************************************************************************/
+    const webpack = {
+        cmd() {
+            return `npm run start`;
+        },
+        description() {
+            return `Runs the webpack project (via ${this.cmd()}) found in ${frontendPath.projectDir()}`;
+        },
+        run() {
+            dir.pushd(frontendPath.projectDir());
 
+            if (program.clean) {
+                nodeClean.run();
+            }
 
-/**************************************************************************************************
- * Commands
- **************************************************************************************************/
+            if (program.restore) {
+                nodeRestore.run();
+            }
 
-// #region Webpack commands
+            echo.message(`Running frontend (via ${this.cmd()})...`);
+            shell.exec(this.cmd(), { silent: false, async: true });
 
-const webpack = {
-    cmd() {
-        return `npm run start`;
-    },
-    description() {
-        return `Runs the webpack project (via ${this.cmd()}) found in ${frontendPath.projectDir()}`;
-    },
-    run() {
-        dir.pushd(frontendPath.projectDir());
+            dir.popd();
+        },
+    };
 
-        if (program.clean) {
-            nodeClean.run();
-        }
-
-        if (program.restore) {
-            nodeRestore.run();
-        }
-
-        echo.message(`Running frontend (via ${this.cmd()})...`);
-        shell.exec(this.cmd(), { silent: false, async: true });
-
-        dir.popd();
-    },
-};
-
-// #endregion Webpack commands
+    // #endregion Webpack commands
 
 
-/**************************************************************************************************
- * Entrypoint / Command router
- **************************************************************************************************/
+    /**************************************************************************************************
+     * Entrypoint / Command router
+     **************************************************************************************************/
 
-// #region Entrypoint / Command router
+    // #region Entrypoint / Command router
 
-program
-    .usage("option(s)")
-    .description(
-        `${commands.webpack.description} Certain options can be chained together for specific behavior` +
-        "(--clean and --restore can be used in conjunction)."
-    )
-    .option("-c, --clean",   nodeClean.description())
-    .option("-p, --publish", webpackPublish.description())
-    .option("-R, --restore", nodeRestore.description())
-    .parse(process.argv);
+    program
+        .usage("option(s)")
+        .description(
+            `${commands.webpack.description} Certain options can be chained together for specific behavior` +
+            "(--clean and --restore can be used in conjunction)."
+        )
+        .option("-c, --clean",   nodeClean.description())
+        .option("-p, --publish", webpackPublish.description())
+        .option("-R, --restore", nodeRestore.description())
+        .parse(process.argv);
 
-// Publish
-if (program.publish) {
-    const result = webpackPublish.run();
-    shell.exit(result ? 0 : 1);
-    return;
-}
+    // Publish
+    if (program.publish) {
+        const result = webpackPublish.run();
+        shell.exit(result ? 0 : 1);
+        return;
+    }
 
-// If no options are passed in, run application
-webpack.run();
+    // If no options are passed in, run application
+    webpack.run();
 
-// #endregion Entrypoint / Command router
+    // #endregion Entrypoint / Command router
+});
