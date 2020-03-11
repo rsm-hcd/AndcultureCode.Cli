64629f4bb310feb03f357b49f412739820872039
Brandon Scott <bscott@andculture.com>
2020-03-11 16:24:21 -0400

    extract-tested Wrapped cli-migration in run()

--- cli-migration.js
+++ cli-migration.js
@@ -1,54 +1,55 @@
 #!/usr/bin/env node
+require("./command-runner").run(async () => {
+    /**************************************************************************************************
+     * Imports
+     **************************************************************************************************/
 
-/**************************************************************************************************
- * Imports
- **************************************************************************************************/
+    const commands  = require("./_modules/commands");
+    const program   = require("commander");
+    const migration = require("./_modules/migration");
 
-const commands  = require("./_modules/commands");
-const program   = require("commander");
-const migration = require("./_modules/migration");
+    /**************************************************************************************************
+     * Variables
+     **************************************************************************************************/
 
-/**************************************************************************************************
- * Variables
- **************************************************************************************************/
+    const modes = migration.modes();
 
-const modes = migration.modes();
+    /**************************************************************************************************
+     * Entrypoint / Command router
+     **************************************************************************************************/
 
-/**************************************************************************************************
- * Entrypoint / Command router
- **************************************************************************************************/
+    // #region Entrypoint / Command router
 
-// #region Entrypoint / Command router
+    program
+        .usage("option")
+        .description(commands.migration.description)
+        .option("-a, --add",     migration.description(modes.ADD))
+        .option("-d, --delete",  migration.description(modes.DELETE))
+        .option("-r, --run",     migration.description(modes.RUN))
+        .parse(process.argv);
 
-program
-    .usage("option")
-    .description(commands.migration.description)
-    .option("-a, --add",     migration.description(modes.ADD))
-    .option("-d, --delete",  migration.description(modes.DELETE))
-    .option("-r, --run",     migration.description(modes.RUN))
-    .parse(process.argv);
+    if (program.add) {
+        migration
+            .mode(modes.ADD)
+            .migrationName(program.args)
+            .run();
+    }
 
-if (program.add) {
-    migration
-        .mode(modes.ADD)
-        .migrationName(program.args)
-        .run();
-}
+    if (program.delete) {
+        migration
+            .mode(modes.DELETE)
+            .migrationName(program.args)
+            .run();
+    }
 
-if (program.delete) {
-    migration
-        .mode(modes.DELETE)
-        .migrationName(program.args)
-        .run();
-}
+    if (program.run) {
+        migration
+            .mode(modes.RUN)
+            .migrationName(program.args)
+            .run();
+    }
 
-if (program.run) {
-    migration
-        .mode(modes.RUN)
-        .migrationName(program.args)
-        .run();
-}
+    if (process.argv.slice(2).length === 0) { program.help(); }
 
-if (process.argv.slice(2).length === 0) { program.help(); }
-
-// #endregion Entrypoint / Command router
+    // #endregion Entrypoint / Command router
+});
