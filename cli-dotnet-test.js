2d3faa779e4b9eba97eb88725a483ebfd652e2dd
Brandon Scott <bscott@andculture.com>
2020-03-12 08:13:41 -0400

    extract-tested Wrapped cli-dotnet-test in run()

--- cli-dotnet-test.js
+++ cli-dotnet-test.js
@@ -1,40 +1,41 @@
 #!/usr/bin/env node
+require("./command-runner").run(async () => {
+    /**************************************************************************************************
+     * Imports
+     **************************************************************************************************/
 
-/**************************************************************************************************
- * Imports
- **************************************************************************************************/
+    const dotnetTest = require("./_modules/dotnet-test");
+    const program    = require("commander");
 
-const dotnetTest = require("./_modules/dotnet-test");
-const program    = require("commander");
+    /**************************************************************************************************
+     * Entrypoint / Command router
+     **************************************************************************************************/
 
-/**************************************************************************************************
- * Entrypoint / Command router
- **************************************************************************************************/
+    // #region Entrypoint / Command router
 
-// #region Entrypoint / Command router
+    program
+        .usage("option")
+        .description(dotnetTest.description())
+        .option("--by-project",     "Runs all test projects for the solution serially")
+        .option("--ci",             "Runs the command in a 'ci' (continuous integration) mode, which provides a summary of failed test projects (only effects --by-project mode)")
+        .option("--coverage",       "Additionally run tests with code coverage via coverlet")
+        .option("-s, --skip-clean", "Skips the clean, build, and restore steps before running the dotnet test runner. This will speed up sequential runs if intentionally running on the same assemblies.")
+        .parse(process.argv);
 
-program
-    .usage("option")
-    .description(dotnetTest.description())
-    .option("--by-project",     "Runs all test projects for the solution serially")
-    .option("--ci",             "Runs the command in a 'ci' (continuous integration) mode, which provides a summary of failed test projects (only effects --by-project mode)")
-    .option("--coverage",       "Additionally run tests with code coverage via coverlet")
-    .option("-s, --skip-clean", "Skips the clean, build, and restore steps before running the dotnet test runner. This will speed up sequential runs if intentionally running on the same assemblies.")
-    .parse(process.argv);
+    // Configure dotnetTest module based on passed in args/options
+    dotnetTest
+        .ciMode(program.ciMode)
+        .filter(program.args)
+        .skipClean(program.skipClean)
+        .withCoverage(program.coverage);
 
-// Configure dotnetTest module based on passed in args/options
-dotnetTest
-    .ciMode(program.ciMode)
-    .filter(program.args)
-    .skipClean(program.skipClean)
-    .withCoverage(program.coverage);
+    if (program.byProject === true) {
+        dotnetTest.runSolutionByProject();
+    }
 
-if (program.byProject === true) {
-    dotnetTest.runSolutionByProject();
-}
+    if (program.byProject == null || !program.byProject) {
+        dotnetTest.runBySolution();
+    }
 
-if (program.byProject == null || !program.byProject) {
-    dotnetTest.runBySolution();
-}
-
-// #endregion Entrypoint / Command router
+    // #endregion Entrypoint / Command router
+});
