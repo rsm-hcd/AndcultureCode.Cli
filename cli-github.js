c4ead52889335d62ce76fb1f5d47b5a09ce126f7
Winton DeShong <contact@winton.me>
2020-06-24 10:48:58 -0400

    Switched cli-github from hardcoded to username param

--- cli-github.js
+++ cli-github.js
@@ -16,14 +16,17 @@ require("./command-runner").run(async () => {
     program
         .usage("option")
         .description(github.description())
+        .option("-u, --username <username>", "Github username for which to list andculture repositories")
         .parse(process.argv);
 
     // Configure github module based on passed in args/options
     echo.success("AndcultureCode Repositories");
     echo.byProperty(await github.repositoriesByAndculture(), "url");
 
-    echo.success("Your Repositories");
-    echo.byProperty(await github.repositoriesByAndculture("wintondeshong"), "url");
+    if (program.username != null) {
+        echo.success(`${program.username} Repositories`);
+        echo.byProperty(await github.repositoriesByAndculture(program.username), "url");
+    }
 
     // #endregion Entrypoint
 });
