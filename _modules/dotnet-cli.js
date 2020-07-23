3da8285fa4f472e92b17402665ba717592c07700
drewcrew11 <dharper@andculture.com>
2020-07-23 15:19:37 -0400

    swapping shell.exec with childprocess.spawnsync

--- _modules/dotnet-cli.js
+++ _modules/dotnet-cli.js
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------------------
 // #region Imports
 // -----------------------------------------------------------------------------------------
-
+const child_process          = require("child_process");
 const dir                    = require("./dir");
 const dotnetBuild            = require("./dotnet-build");
 const dotnetPath             = require("./dotnet-path");
@@ -25,15 +25,20 @@ const { red, tabbedNewLine } = formatters;
 // -----------------------------------------------------------------------------------------
 
 const dotnetCli = {
-    cmd() {
-        if (!dotnetPath.cliPath()) return "";
-        return `dotnet ${path.basename(dotnetPath.cliPath())}`;
+    cmd(cliArgs) {
+        return {
+            args: [path.basename(dotnetPath.cliPath())].concat(cliArgs.split(" ")),
+            cmd: "dotnet",
+            toString() {
+                return `${this.cmd} ${this.args.join(" ")}`;
+            },
+        };
     },
     description() {
-        return `Shortcut that forwards any/all LMS Dotnet Cli commands to be run in the correct location in the project (via ${this.cmd()}) ` +
+        return `Shortcut that forwards any/all LMS Dotnet Cli commands to be run in the correct location in the project (via ${this.cmd("")}) ` +
             tabbedNewLine(red("NOTE: ") + "Arguments need to be wrapped in quotes, ie \"test database migrate\"");
     },
-    run(args) {
+    run(cliArgs) {
         const cliDir = dotnetPath.cliDir();
 
         // Build dotnet project if the *Cli.dll is not found
@@ -45,12 +50,16 @@ const dotnetCli = {
         dir.pushd(dotnetPath.cliDir());
 
         // Dynamically find the latest dotnet core bin so that upgrades won't break this command
-        const fullCommand = `${this.cmd()} ${args}`;
-        echo.success(`Full command:` + fullCommand);
-        const {code} = shell.exec(fullCommand);
-        if (code !== 0) {
+
+        const { cmd, args } = this.cmd(cliArgs);
+        
+        echo.success(`Full command:` + this.cmd(cliArgs).toString());
+        //const {code} = shell.exec(fullCommand);
+        const { status } = child_process.spawnSync(cmd, args, { stdio: "inherit", shell: true });
+
+        if (status !== 0) {
             echo.error("Command failed, see output for details.");
-            shell.exit(code);
+            shell.exit(status);
         }
 
         dir.popd();
