39e9739074c6f5930adc60530bc86f358e8d594b
Brandon Scott <brandongregoryscott@gmail.com>
2020-08-24 08:27:20 -0400

    Cleanup formatting in dotnet-cli module, remove unnecessary comments

--- _modules/dotnet-cli.test.js
+++ _modules/dotnet-cli.test.js
@@ -1,65 +1,67 @@
 // -----------------------------------------------------------------------------------------
 // #region Imports
 // -----------------------------------------------------------------------------------------
+
 const child_process = require("child_process");
-const dotnetCli     = require("./dotnet-cli");
-const shell         = require("shelljs");
-const dotnetPath    = require("./dotnet-path");
-const dotnetBuild   = require("./dotnet-build");
-const faker         = require("faker");
+const dotnetBuild = require("./dotnet-build");
+const dotnetCli = require("./dotnet-cli");
+const dotnetPath = require("./dotnet-path");
+const faker = require("faker");
+const shell = require("shelljs");
 
 // #endregion Imports
-
 
 // -----------------------------------------------------------------------------------------
 // #region Tests
 // -----------------------------------------------------------------------------------------
 
 describe("dotnetCli", () => {
+    // -----------------------------------------------------------------------------------------
+    // #region run
+    // -----------------------------------------------------------------------------------------
 
-  // -----------------------------------------------------------------------------------------
-  // #region run
-  // -----------------------------------------------------------------------------------------
-  describe("run", () => {
-    let shellExitSpy;
-    beforeEach(() => {
-        shellExitSpy = jest.spyOn(shell, "exit").mockImplementation();
+    describe("run", () => {
+        let shellExitSpy;
+        beforeEach(() => {
+            shellExitSpy = jest.spyOn(shell, "exit").mockImplementation();
+        });
+
+        test(`when dotnetPath.cliDir() path returns undefined it calls donetBuild.run`, () => {
+            // Arrange
+            const dotnetPathSpy = jest
+                .spyOn(dotnetPath, "cliDir")
+                .mockImplementation(() => undefined);
+            const dotnetBuildSpy = jest
+                .spyOn(dotnetBuild, "run")
+                .mockImplementation();
+
+            // Act
+            dotnetCli.run();
+
+            // Assert
+            expect(dotnetPathSpy).toHaveBeenCalled();
+            expect(dotnetBuildSpy).toHaveBeenCalled();
+        });
+
+        test(`when spawn.sync returns non 0 it calls shell.exit with the status`, () => {
+            // Arrange
+            const exitCode = faker.random.number({ min: 1 });
+            const spawnSync = jest
+                .spyOn(child_process, "spawnSync")
+                .mockImplementation(() => {
+                    return { status: exitCode };
+                });
+
+            // Act
+            dotnetCli.run();
+
+            // Assert
+            expect(spawnSync).toHaveBeenCalled();
+            expect(shellExitSpy).toHaveBeenCalledWith(exitCode);
+        });
     });
 
-    test(`when dotnetPath.cliDir() path returns undefined it calls donetBuild.run`, () => {
-        // Arrange
-        const dotnetPathSpy  = jest.spyOn(dotnetPath,  "cliDir").mockImplementation(() => undefined);
-        const dotnetBuildSpy = jest.spyOn(dotnetBuild, "run").mockImplementation();
-
-         // Act
-         dotnetCli.run();
-
-         // Assert
-         expect(dotnetPathSpy).toHaveBeenCalled();
-         expect(dotnetBuildSpy).toHaveBeenCalled();
-    });
-
-    //TODO: test if spawn sync returns a non 0 status
-    // it calls shell.exit with the status
-    test(`when spawn.sync returns non 0 it calls shell.exit with the status`, () => {
-      // Arrange
-      const exitCode  = faker.random.number({ min: 1 });
-      const spawnSync = jest.spyOn(child_process, "spawnSync").mockImplementation(() => {
-        return {status: exitCode};
-      });
-
-      // Act
-      dotnetCli.run();
-
-      // Assert
-      expect(spawnSync).toHaveBeenCalled();
-      expect(shellExitSpy).toHaveBeenCalledWith(exitCode);
-    });
-
-  });
-
-
-  // #endregion run
+    // #endregion run
 });
 
 // #endregion Tests
